b7df0d5d572e4f1b49b4534b1c7602b9742ce202
ThanSweLin <thanswelin2015@gmail.com>
2021-09-23 20:03:38 +0630

    Added Files

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = '70eff57c9ab5b5a860363e56df81f89a'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'blogblog353@gmail.com'
-    MAIL_PASSWORD = 'blog%353Blog%353#'
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
